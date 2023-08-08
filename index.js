af49f16b94e0974d289fe269239c3a3e37329168
Marcos Roberto Albrecht <marquinhos.beto@hotmail.com>
2023-08-08 10:34:18 -0300

    first commit

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-	 console.log('new connection');
-   console.log('id socket', socket.id);
+	console.log('new connection');
+  console.log('id socket', socket.id);
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    console.log('mensagem: ', msg)
   });
 });
 
